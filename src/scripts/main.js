1c04fb60bc886f1468229d732aa70b9ceeae01f8
Eduardo Higareda <eldelentes@gmail.com>
2018-02-01 12:00:53 -0600

    Adds arro functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,11 +12,22 @@ function init(){
       onlyInViewport: false,
     },
   });
+
   $('.swiper-slide-active .effect').addClass('is-active');
   mySwiper.on('transitionEnd', function () {
     $('.swiper-slide-active .effect').addClass('is-active');
     $('.swiper-slide-prev .effect, .swiper-slide-next .effect').removeClass('is-active');
   });
+
+  $(document).on( "click", function() {
+    if ($('body').hasClass('is-right')) {
+      mySwiper.slideNext();
+    }
+
+    if ($('body').hasClass('is-left')) {
+      mySwiper.slidePrev();
+    }
+  })
 }
 
 pageLink.click(function(){
@@ -35,4 +46,28 @@ $( document ).ajaxStart(function() {
 
 $(document).ajaxComplete(function () {
   init();
+});
+
+
+var windowTop = 58;
+var windowBottom = $(window).height() - 58;
+var windowWidth = $(window).width();
+var windowHalf = windowWidth / 2;
+
+$(document).mousemove(function( event ) {
+  var mouseX = event.pageX;
+  var mouseY = event.pageY;
+  if (mouseX < windowHalf && mouseY > windowTop && mouseY < windowBottom) {
+    $('body').addClass('is-left');
+    $('body').removeClass('is-right');
+  } else {
+    $('body').removeClass('is-left');
+  }
+
+  if (mouseX > windowHalf && mouseY > windowTop && mouseY < windowBottom) {
+    $('body').addClass('is-right');
+    $('body').removeClass('is-left');
+  } else {
+   $('body').removeClass('is-right');
+  }
 });
