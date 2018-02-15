c68eb1effdd997686eb18e813d9541625fb4b0b9
Eduardo Higareda <eldelentes@gmail.com>
2018-02-15 11:25:13 -0600

    Fix multiple errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -118,13 +118,13 @@ pageLink.click(function(){
   mySwiper.slideTo(slide);
 })
 
-$('.main-header__brand').click(function(){
-  mySwiper.slideTo(0);
-})
+// $('.main-header__brand').click(function(){
+//   mySwiper.slideTo(0);
+// })
 
 
 mySwiper = new Swiper('.swiper-container', {
-
+  speed: 600,
   allowTouchMove: false,
   keyboard: {
     enabled: true,
